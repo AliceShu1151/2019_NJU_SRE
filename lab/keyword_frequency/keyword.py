39a87462a7760748a79e59324ecd5e54b6bed64d
wuyuxin <876465382@qq.com>
2019-10-14 22:24:02 +0800

    根据关键字将pr标题分类

--- lab/keyword_frequency/keyword.py
+++ lab/keyword_frequency/keyword.py
@@ -17,7 +17,7 @@ keywords_list=[]
 for key,word in keywords_set:
     keywords_list.append((key,word))
 keywords_list.sort(key=lambda element: element[1],reverse = True)
-print(keywords_list)
+# print(keywords_list)
 
 target_file_name = "keywords_list.csv"
 target_file = open(target_file_name,mode='w')
@@ -32,3 +32,28 @@ for word,frequency in keywords_list:
     target_file.write('\n')
 
 target_file.close()
+
+#读取pr标题存入列表
+pr_title_list = []
+with open(file_name) as f:
+    for line in f:
+        # print(line.__len__())
+        pr_title_list.append(line)
+
+#根据关键字将pr标题分类
+classified_file = open("classified_pull_request.txt",mode='w')
+for word,frequency in keywords_list:
+    if frequency<5:
+        break
+    classified_file.write(word)
+    classified_file.write(':')
+    classified_file.write('\n')
+    for line in pr_title_list:
+        word_in_line_list=line.split()
+        if word in word_in_line_list:
+            classified_file.write('\t')
+            classified_file.write(line)
+    classified_file.write('\n')
+classified_file.close()
+
+    
