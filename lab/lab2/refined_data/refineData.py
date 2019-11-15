bdc43e04a85e1d2d985a48bd93cb6788c2641394
Mi-racle <54791981+Mi-racle@users.noreply.github.com>
2019-11-15 09:47:21 +0800

    Add files via upload

--- lab/lab2/refined_data/refineData.py
+++ lab/lab2/refined_data/refineData.py
@@ -1,6 +1,13 @@
-file_names[] = {"eclipse_bugs_1.csv"
-                '''
+file_names = [  "eclipse_bugs_1.csv",
                 "eclipse_bugs_10000.csv",
+                "eclipse_bugs_20000.csv",
+                "eclipse_bugs_30000.csv",
+                "eclipse_bugs_40000.csv",
+                "eclipse_bugs_50000.csv",
+                "eclipse_bugs_60000.csv",
+                "eclipse_bugs_70000.csv",
+                "eclipse_bugs_80000.csv",
+                "eclipse_bugs_90000.csv",
                 "eclipse_bugs_100000.csv",
                 "eclipse_bugs_110000.csv",
                 "eclipse_bugs_120000.csv",
@@ -11,7 +18,6 @@ file_names[] = {"eclipse_bugs_1.csv"
                 "eclipse_bugs_170000.csv",
                 "eclipse_bugs_180000.csv",
                 "eclipse_bugs_190000.csv",
-                "eclipse_bugs_20000.csv",
                 "eclipse_bugs_200000.csv",
                 "eclipse_bugs_210000.csv",
                 "eclipse_bugs_220000.csv",
@@ -22,7 +28,6 @@ file_names[] = {"eclipse_bugs_1.csv"
                 "eclipse_bugs_270000.csv",
                 "eclipse_bugs_280000.csv",
                 "eclipse_bugs_290000.csv",
-                "eclipse_bugs_30000.csv",
                 "eclipse_bugs_300000.csv",
                 "eclipse_bugs_310000.csv",
                 "eclipse_bugs_320000.csv",
@@ -33,7 +38,6 @@ file_names[] = {"eclipse_bugs_1.csv"
                 "eclipse_bugs_370000.csv",
                 "eclipse_bugs_380000.csv",
                 "eclipse_bugs_390000.csv",
-                "eclipse_bugs_40000.csv",
                 "eclipse_bugs_400000.csv",
                 "eclipse_bugs_410000.csv",
                 "eclipse_bugs_420000.csv",
@@ -44,24 +48,54 @@ file_names[] = {"eclipse_bugs_1.csv"
                 "eclipse_bugs_470000.csv",
                 "eclipse_bugs_480000.csv",
                 "eclipse_bugs_490000.csv",
-                "eclipse_bugs_50000.csv",
                 "eclipse_bugs_500000.csv",
                 "eclipse_bugs_510000.csv",
                 "eclipse_bugs_520000.csv",
                 "eclipse_bugs_530000.csv",
                 "eclipse_bugs_540000.csv",
-                "eclipse_bugs_550000.csv",
-                "eclipse_bugs_60000.csv",
-                "eclipse_bugs_70000.csv",
-                "eclipse_bugs_80000.csv",
-                "eclipse_bugs_90000.csv"
-                '''
-                }
+                "eclipse_bugs_550000.csv"
+              ]
 
+path = "data/"
+path_r = "refined_data/"
+
+import csv
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+sr = stopwords.words('english')
+wnl = WordNetLemmatizer()
 for file_name in file_names:
-    file_in = open(file_name)
-    lines = file_in.read().splitlines()
-
-for line in lines:
-    print(line + '\n')
-    
+    with open(path + file_name) as fr:
+        reader = csv.reader(fr)
+        with open(path_r + "refined" + file_name, 'w', newline='') as fw:
+            writer = csv.writer(fw)
+            lines = []
+            for row in reader:
+                if row[0] == "id":
+                    headers = [row[0], row[1], row[2], row[7]] # 0 id, 1 title, 2 status, 7 importance
+                    lines.append(headers)
+                else:
+                    if not row[0].isdigit():
+                        continue
+                    line = []
+                    line.append(row[0])
+                    words = row[1].split()
+                    tmp_words = []
+                    for word_origin in words:
+                        word_lower = word_origin.lower()
+                        if word_lower in sr: # word_origin is a stopword
+                            continue
+                        else:
+                            word = wnl.lemmatize(word_lower, pos = 'v')
+                            if not word.isalpha():
+                                continue
+                            tmp_words.append(word)
+                    line.append(' '.join(tmp_words))
+                    line.append(row[2])
+                    line.append(row[7])
+                    lines.append(line)
+            writer.writerows(lines)
+            fw.close()
+        fr.close()
